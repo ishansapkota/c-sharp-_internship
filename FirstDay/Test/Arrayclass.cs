4573b9677c11d0d93044cb0215b5a5d787e2fd73
PERCOIDIT <ishansapkota2@gmail.com>
2024-06-04 13:49:26 +0545

    Deleted the empty folder

---
+++ FirstDay/Test/Arrayclass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test
+{
+    class Arrayclass
+    {
+        string[] names = new string[3];
+
+        public void GetName()
+        {
+            for(int i=0;i<3;i++)
+            {
+                names[i] = Console.ReadLine();
+            }
+            
+        }
+
+        public void DisplayName()
+        {
+            for(int i=0;i<3;i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+        }
+    }
+    
+ 
+}
---
+++ FirstDay/Test/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+            //Normal class and object problem
+            /*Class1 class1 = new Class1();
+            class1.getInput();
+            class1.getOutput();*/
+
+            //Utilizing inheritance
+            /*   Parent parentobj = new Parent();
+               parentobj.getInfo();
+               parentobj.showInfo();
+               parentobj.eat();
+               parentobj.sleep();
+
+               Child childobj = new Child();
+               childobj.getInfo();
+               childobj.showInfo();
+               childobj.eat();
+               childobj.sleep();*/
+
+            /*Arrayclass[] arrayclass = new Arrayclass[2];
+            for (int i=0;i<2;i++)
+            {
+                arrayclass[i] = new Arrayclass();
+                arrayclass[i].GetName();
+            }
+
+            Console.WriteLine("---------------------");
+
+            for(int i=0;i<2;i++)
+            {
+                arrayclass[i].DisplayName();
+            }
+*/
+
+            Arrayclass arrayclass = new Arrayclass();
+            arrayclass.GetName();
+            arrayclass.DisplayName();
+        }
+    }
+}
